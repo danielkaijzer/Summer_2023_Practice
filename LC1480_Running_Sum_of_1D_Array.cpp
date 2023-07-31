4abb7f8c162c2ad47421b0104f82cbbf8c7b8625
danielkaijzer <danielkaijzer00@gmail.com>
2023-07-31 17:59:04 -0400

    Add New File

---
+++ LC1480_Running_Sum_of_1D_Array.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file LC1480_Running_Sum_of_1D_Array.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-07-31
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> runningSum(std::vector<int>& nums) {
+    
+}
+
+
+// Driver
+int main(){
+
+    std::vector<int> nums = {9,3,15,20,7};
+}
