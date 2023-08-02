2b856867170e8962daf254cd19205f021b6f1034
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-01 23:06:02 -0400

    Updated code, not yet solved

--- LC844_Backspace_String_Compare.cpp
+++ LC844_Backspace_String_Compare.cpp
@@ -2,7 +2,7 @@
  * @file LC844_Backspace_String_Compare.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2023-08-01
  * 
  * @copyright Copyright (c) 2023
@@ -11,7 +11,27 @@
 
 #include <iostream>
 
+// Takes a string with backspace chars and removes chars previous to backspace chars
+std::string backspaceOutput(std::string s){
+    std::string output;
+
+    for (int i = 0; i < s.size(); i++){
+        // if char != # then we add to string
+
+        // if char == #, we remove last entry to string
+    }
+
+    std::cout << output << std::endl;
+    return output;
+
+}
+
 bool backspaceCompare(std::string s, std::string t) {
+
+    // Process both strings
+
+    // compare outputs
+    return (backspaceOutput(s) == backspaceOutput(t));
     
 }
 
