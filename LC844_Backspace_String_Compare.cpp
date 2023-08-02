ac41de96ca5d096f00f2c30580abf597d815819a
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-01 22:54:49 -0400

    Setup for LC844, not yet solved

---
+++ LC844_Backspace_String_Compare.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file LC844_Backspace_String_Compare.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-01
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+
+bool backspaceCompare(std::string s, std::string t) {
+    
+}
+
+
+// Driver
+int main(){
+
+    std::string s = "ab#c";
+    std::string t = "ad#c";
+
+
+}
