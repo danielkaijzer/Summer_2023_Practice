dd0c183b9b69660dd3421de41a4a5dcff3bf59a5
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-03 01:16:21 -0400

    First solution to LC15 completed

--- LC15_3Sum.cpp
+++ LC15_3Sum.cpp
@@ -7,7 +7,7 @@
  * 
  * Notice that the solution set must not contain duplicate triplets.
  * 
- * @version 0.1
+ * @version v1
  * @date 2023-08-02
  * 
  * @copyright Copyright (c) 2023
@@ -17,42 +17,74 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
+
+/*
+ideas:
+- brute force every combination with 3 nested loops
+- iterate though hashmap with triple nested loop
+- store every unique combination in hashmap of 2D vectors
+- 
+
+*/
+
+
 vector<vector<int>> threeSumHelper(vector<int>& nums, unordered_map<int,int> hmp){
 
-    vector<vector<int>> output = {{}};
+    vector<vector<int>> solution;
 
-    for (int i = 0; i <nums.size(); i++){
-        for (int j = i + 1; j <nums.size(); j++){
-            int current_2sum = nums[i] + nums[j];
-            int complement = 0 - current_2sum;
-
-            if (hmp.find(complement) != hmp.end()){
-                // if triplet exists, add it to output 2D vector of ints
-                output.push_back({nums[i],nums[j],complement});
-            }
+    
+    for (int i = 0; i <nums.size()-2; i++){ // added -2 since we only look at cur value + next two
+        if (nums[i] >0){ // no solution for all positive 3sum
+            break;
         }
+
+        for (int j = i + 1; j <nums.size()-1; ++j){
+            int complement = -1*(nums[i] + nums[j]);
+
+            // make sure complement (k value for solution) exists
+            // if it does, make sure j value is last occurrence 
+            // to avoid duplicate triplets
+            if (hmp.find(complement) != hmp.end() && hmp.find(complement)->second > j){
+                solution.push_back({{nums.at(i), nums.at(j), complement}});
+            }
+            // update j index to be last occurrence to avoid triplets
+            j = hmp.find(nums[j])->second;
+        }
+        // update i index to be last occurrence to avoid triplets
+        i = hmp.find(nums[i])->second;
     }
 
-    return output;
+    return solution;
 }
 
 vector<vector<int>> threeSum(vector<int>& nums) {
+    // first sort vector
+    sort(nums.begin(), nums.end());
+    
     // we want to try all combinations of the numbers such that all three number indices are unique and they add up to 0.
-    if (nums.size() ==3){
+    if (nums.size() == 3){
         if (nums.at(0) + nums.at(1) + nums.at(2) == 0){
             return {{nums.at(0), nums.at(1), nums.at(2)}};
         }
-        return {{}};
+        return {};
+    }
+    else if (nums.size() < 3){ // no triplet solution if vector size < 3
+        return {};
+    }
+
+    if (nums[0] > 0){ // if we have no zero or negatives, no solution
+        return {};
     }
 
     // store numbers in a hashmap
     unordered_map<int,int> hmp;
 
-    for (int i=0;i<nums.size();i++){
-        hmp[i] = nums.at(i);
+    for (int i = 0; i < nums.size(); ++i){
+        hmp[nums[i]] = i;
     }
 
     return threeSumHelper(nums,hmp);
