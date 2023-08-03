42e7c41a4cb07e9cebc18ac29e16c106c7155384
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-03 17:37:28 -0400

    Just getting started on this problem.

---
+++ LC482_License_Key_Format.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file LC482_License_Key_Format.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+string licenseKeyFormatting(string s, int k) {
+    // if (s.length() <k){
+    //     return s;
+    // }
+    
+}
+
+int main(){
+
+int key = 4;
+string s = "5F3Z-2e-9-w";
+
+cout << licenseKeyFormatting(s, key) << endl;
+
+}
