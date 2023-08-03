54c4fac1c2af7958d107f9c81ac122d302815b03
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-02 23:11:25 -0400

    LeetCode Problem 15. Just starting. Not yet solved.

---
+++ LC15_3Sum.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file LC15_3Sum.cpp
+ * @author Daniel Kaijzer
+ * @brief Given an integer array nums, return all the triplets 
+ * [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, 
+ * and nums[i] + nums[j] + nums[k] == 0.
+ * 
+ * Notice that the solution set must not contain duplicate triplets.
+ * 
+ * @version 0.1
+ * @date 2023-08-02
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> threeSum(vector<int>& nums) {
+    
+}
+
+int main(){
+    vector<int> input = {-1,0,1,2,-1,-4};
+
+
+
+}
