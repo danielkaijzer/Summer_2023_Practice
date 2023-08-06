06ff605033db7e53ff9ca0886b58c310d736fa81
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-05 21:48:33 -0400

    First solution works for more test cases

--- LC482_License_Key_Format.cpp
+++ LC482_License_Key_Format.cpp
@@ -2,12 +2,15 @@
  * @file LC482_License_Key_Format.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 1
+ * @version 2
  * @date 2023-08-03
  * 
  * @copyright Copyright (c) 2023
  * 
  */
+
+// What I need to fix: 
+// Prioritize splitting all alphanum groups into parts with k chars each
 
 #include <iostream>
 #include <unordered_map>
@@ -15,21 +18,30 @@
 using namespace std;
 
 string licenseKeyFormatting(string s, int k) {
-    string output;
 
-    // first add all chars up to and including first '-'
+    string output;
 
     int i = 0;
 
-    // add all chars up to first '-'
-    while (s[i] != '-'){
+    // check alphanum count to determine whether to skip first dash
+    int alphanum_count =0;
+    for (char c: s){
+        if (c != '-'){
+            alphanum_count++;
+        }
+    }
+
+    if (alphanum_count%k != 0){
+        // add all chars up to first '-'
+        while (s[i] != '-'){
+            output.push_back(s[i]);
+            i++;
+        }
+
+        // add first '-'
         output.push_back(s[i]);
         i++;
     }
-
-    // add first '-'
-    output.push_back(s[i]);
-    i++;
 
     int dash_index = 1;
 
@@ -58,26 +70,19 @@ string licenseKeyFormatting(string s, int k) {
     if (output[output.size()-1] == '-'){
         output.pop_back();
     }
-
-    // for (char c: s){
-
-
-    //     cout << c << endl;
-    // }
-
-    // add dashes for every 
-
     return output;
 }
 
 int main(){
 
 // int key = 4;
-string s1 = "5F3Z-2e-9-w";
-string s2 = "2-5G-3J";
+string s1 = "5F3Z-2e-9-w"; 
+string s2 = "2-5G-3J"; // since 
+string s3 = "2-4A0r7-4k"; // since there are 8 alphanum chars, where k=4, ignore first dash
 
 
-cout << licenseKeyFormatting(s1, 4) << endl;
-cout << licenseKeyFormatting(s2, 2) << endl;
+cout << licenseKeyFormatting(s1, 4) << endl; // "5F3Z-2E9W"
+cout << licenseKeyFormatting(s2, 2) << endl; // "2-5G-3J"
+cout << licenseKeyFormatting(s3, 4) << endl; // "24A0-R74K"
 
 }
