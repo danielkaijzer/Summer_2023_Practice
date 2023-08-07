8bf979c81dc01389ba1b9d1ec6794ab806a662dc
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-06 23:19:32 -0400

    First version of design but has an error

--- LC622_Design_Circular_Queue.cpp
+++ LC622_Design_Circular_Queue.cpp
@@ -2,7 +2,7 @@
  * @file LC622_Design_Circular_Queue.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2023-08-06
  * 
  * @copyright Copyright (c) 2023
@@ -20,14 +20,16 @@ private:
     int front_;
     int rear_;
     int capacity_;
+    int item_count_;
 
 public:
     // Constructor
     MyCircularQueue(int capacity) {
         capacity_ = capacity;
         data_ = new int[capacity_];
-        front_ = -1;
-        rear_ = -1;
+        front_ = -1; // see deQueue for reasoning behind this start value
+        rear_ = -1;  // see enQueue for reasoning behind this start value
+        item_count_ = 0;
     }
 
     // Destructor
@@ -36,41 +38,53 @@ public:
     }
     
     bool enQueue(int value) {
-        return false;
+        if (isFull()){
+            return false;
+        }
+        rear_ = (rear_+1)%capacity_;
+        data_[rear_] = value;
+        item_count_++;
+        return true;
     }
     
     bool deQueue() {
-        return false;
+        if (isEmpty()){
+            return false;
+        }
+        front_ = (front_+1) %capacity_;
+        item_count_--;
+        return true;
     }
     
     int Front() {
-        return -1;
+        return data_[front_];
     }
     
     int Rear() {
-        return -1;
+        return data_[rear_];
     }
     
     bool isEmpty() {
-        return false;
+        return (item_count_ == 0);
     }
     
     bool isFull() {
-        return false;
+        return (!isEmpty());
     }
 };
 
 int main(){
-    int k = 1;
-    int value = 1;
 
-    MyCircularQueue* obj = new MyCircularQueue(k);
-    bool param_1 = obj->enQueue(value);
-    bool param_2 = obj->deQueue();
-    int param_3 = obj->Front();
-    int param_4 = obj->Rear();
-    bool param_5 = obj->isEmpty();
-    bool param_6 = obj->isFull();
+    MyCircularQueue myCircularQueue = new MyCircularQueue(3);
+    cout << myCircularQueue.enQueue(1); // return True
+    cout << myCircularQueue.enQueue(2); // return True
+    cout << myCircularQueue.enQueue(3); // return True
+    cout << myCircularQueue.enQueue(4); // return False
+    cout << myCircularQueue.Rear();     // return 3
+    cout << myCircularQueue.isFull();   // return True
+    cout << myCircularQueue.deQueue();  // return True
+    cout << myCircularQueue.enQueue(4); // return True
+    cout << myCircularQueue.Rear();     // return 4
 }
 
 /**
