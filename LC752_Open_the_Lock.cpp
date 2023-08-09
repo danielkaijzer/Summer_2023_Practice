aac352423f06c530957d83e88dd3a2a3500510d7
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-09 14:08:00 -0400

    Tried a solution from leetcode, having trouble understanding BFS

--- LC752_Open_the_Lock.cpp
+++ LC752_Open_the_Lock.cpp
@@ -2,8 +2,8 @@
  * @file LC752_Open_the_Lock.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.2
- * @date 2023-08-08
+ * @version 0.3
+ * @date 2023-08-09
  * 
  * @copyright Copyright (c) 2023
  * 
@@ -17,27 +17,64 @@
 using namespace std;
 
 int openLock(vector<string>& deadends, string target) {
-    
     string start = "0000";
-    unordered_set<string> visited;
     
-    for (auto s : deadends){
-        if (s == start){ // start point is in deadends, return -1
-            return -1; 
-        }
-        // add deadends to visited
-        visited.insert(s);
-    }
+    // if target is start point
+    if (target == start) return 0;
 
-    queue<string> q;
-    int step = 0;
+    // initialize hash set for visited configs, include deadends
+    unordered_set<string> visited(begin(deadends), end(deadends));
 
-    q.push(start); // add start point to queue
+    // if start is a deadend
+    if (visited.count(start)) return -1;
+
+    // initialize queue object
+    queue<string> q(deque<string> {start});
+
+    string main, cur, prev;
+
+    int size = q.size();
+    int level =1;
 
 
     // BFS
+
     while (!q.empty()){
 
+        main = cur = prev = q.front(); 
+        q.pop();
+
+        for (int i =0; i<4;i++){
+            if (cur[i] == '9'){
+                cur[i] += -9;
+            }
+            else{
+                cur[i] += 1;
+            }
+            if (cur == target) return level;
+            if (!visited.count(cur)){
+                q.push(cur);
+                visited.insert(cur);
+            }
+
+
+            if (prev[i] == '0'){
+                prev[i] -= -9;
+            }
+            else{
+                prev[i] -= 1;
+            }
+            if (prev == target) return level;
+            if (!visited.count(prev)){
+                q.push(prev);
+                visited.insert(prev);
+            }
+
+            cur = prev = main;
+        }
+    
+        size--;
+        level++;
     }
 
 
@@ -51,6 +88,6 @@ int main(){
     vector<string> deadends = {"0201","0101","0102","1212","2002"};
     string target = "0202";
 
-    cout << openLock(deadends, target);
+    cout << openLock(deadends, target) << endl;
 
 }
