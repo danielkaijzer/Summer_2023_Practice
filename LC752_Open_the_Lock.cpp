96841720b28327d0a8bb27fe5f583eb8b40845cc
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-08 23:25:38 -0400

    Setup file for LC752 BFS Queue practice problem

---
+++ LC752_Open_the_Lock.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file LC752_Open_the_Lock.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-08
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// helper function to perform dfs
+void dfs(vector<vector<char>>& grid, int i, int j){
+
+}
+
+int openLock(vector<string>& deadends, string target) {
+    return -1;
+    
+}
+
+int main(){
+    vector<string> deadends = {"0201","0101","0102","1212","2002"};
+    string target = "0202";
+
+    cout << openLock(deadends, target);
+
+}
