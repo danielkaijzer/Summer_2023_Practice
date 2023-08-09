b9b70c28503a989c9b8105dd4e61c56037e754a4
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-08 23:55:27 -0400

    More setup

--- LC752_Open_the_Lock.cpp
+++ LC752_Open_the_Lock.cpp
@@ -2,7 +2,7 @@
  * @file LC752_Open_the_Lock.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2023-08-08
  * 
  * @copyright Copyright (c) 2023
@@ -11,16 +11,39 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
+#include <unordered_set>
 
 using namespace std;
 
-// helper function to perform dfs
-void dfs(vector<vector<char>>& grid, int i, int j){
-
-}
-
 int openLock(vector<string>& deadends, string target) {
-    return -1;
+    
+    string start = "0000";
+    unordered_set<string> visited;
+    
+    for (auto s : deadends){
+        if (s == start){ // start point is in deadends, return -1
+            return -1; 
+        }
+        // add deadends to visited
+        visited.insert(s);
+    }
+
+    queue<string> q;
+    int step = 0;
+
+    q.push(start); // add start point to queue
+
+
+    // BFS
+    while (!q.empty()){
+
+    }
+
+
+
+
+    return -1; // if target not found
     
 }
 
