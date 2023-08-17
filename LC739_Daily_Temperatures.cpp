b67cbbe23bab913800f84f9ce32f672fa72bc893
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-16 23:33:00 -0400

    Setup file

---
+++ LC739_Daily_Temperatures.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file LC739_Daily_Temperatures.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-08-16
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    return {};
+}
+
+
+int main(){
+
+}
