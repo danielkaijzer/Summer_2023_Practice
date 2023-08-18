98291d6b33c5655d7ca8b1cf85b17b5edd552437
danielkaijzer <danielkaijzer00@gmail.com>
2023-08-17 22:58:44 -0400

    First attempt for a solution, brute force

--- LC739_Daily_Temperatures.cpp
+++ LC739_Daily_Temperatures.cpp
@@ -2,7 +2,7 @@
  * @file LC739_Daily_Temperatures.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2023-08-16
  * 
  * @copyright Copyright (c) 2023
@@ -11,12 +11,26 @@
 
 #include <iostream>
 #include <stack>
+#include <unordered_map>
 #include <vector>
 
 using namespace std;
 
 vector<int> dailyTemperatures(vector<int>& temperatures) {
-    return {};
+    vector<int> output;
+
+    // add all elements to hashmap
+    for (int i = 0; i < temperatures.size(); i++){
+        int j = i;
+        // find num of days until higher temp
+
+        while (temperatures[i] >= temperatures[j] && j < temperatures.size()){
+            j++;
+        }
+        output.push_back(j-i);
+    }
+
+    return output;
 }
 
 
