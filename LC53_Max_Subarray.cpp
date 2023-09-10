dec4598aa257c1da6618f65fcd0b6b975eeace4b
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-10 16:01:18 -0400

    Setup for LC53

---
+++ LC53_Max_Subarray.cpp
@@ -0,0 +1,27 @@
+/**
+ * @file LC53_Max_Subarray.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-10
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxSubArray(vector<int>& nums) {
+    int max_sum = 0;
+    int current_sum = 0;
+}
+
+
+int main(){
+    vector<int> nums1{-2,1,-3,4,-1,2,1,-5,4};
+
+    cout << maxSubArray(nums1) << endl;
+}
