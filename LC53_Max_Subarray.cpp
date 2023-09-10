c94281bc071ed9fc69063d34cbe9301e018aa929
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-10 16:05:47 -0400

    First attempt for solution

--- LC53_Max_Subarray.cpp
+++ LC53_Max_Subarray.cpp
@@ -2,7 +2,7 @@
  * @file LC53_Max_Subarray.cpp
  * @author Daniel Kaijzer
  * @brief 
- * @version 0.1
+ * @version 0.2
  * @date 2023-09-10
  * 
  * @copyright Copyright (c) 2023
@@ -17,11 +17,24 @@ using namespace std;
 int maxSubArray(vector<int>& nums) {
     int max_sum = 0;
     int current_sum = 0;
+
+    for (int i = 0; i < nums.size(); i++){
+        current_sum += nums[i];
+
+        if (current_sum > max_sum){
+            max_sum = current_sum;
+        }
+        else if (current_sum < 0){
+            current_sum = 0;
+        }
+    }
+
+    return max_sum;
 }
 
 
 int main(){
-    vector<int> nums1{-2,1,-3,4,-1,2,1,-5,4};
+    vector<int> nums1{-2,1,-3,4,-1,2,1,-5,4}; // 6 is answer
 
     cout << maxSubArray(nums1) << endl;
 }
