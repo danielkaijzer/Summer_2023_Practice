7268abb36e2759fbd78493958f6762ca739b0db6
danielkaijzer <danielkaijzer00@gmail.com>
2023-09-12 23:04:00 -0400

    Setup file

---
+++ LC66_Plus_One.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file LC66_Plus_One.cpp
+ * @author Daniel Kaijzer
+ * @brief 
+ * @version 0.1
+ * @date 2023-09-12
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits) {
+    return {};
+}
+
+
+int main(){
+    vector<int> nums1{0,1,0}; // 1 is answer
+
+    cout << plusOne(nums1).at(0) << endl;
+}
